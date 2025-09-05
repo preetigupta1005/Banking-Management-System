d6df2f5e45b30b203555995f263f0f20c6c6adac
Preeti Gupta <your-email@example.com>
2025-09-06 01:01:55 +0530

    Initial commit

---
+++ workspace/springboot/src/main/mainclass.java
@@ -0,0 +1,5 @@
+package main;
+ import org
+public class mainclass {
+
+}
---
+++ workspace/springboot/src/spdc/employee.java
@@ -0,0 +1,20 @@
+package spdc;
+
+public class employee {
+	private String name;
+	private int empcode;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getEmpcode() {
+		return empcode;
+	}
+	public void setEmpcode(int empcode) {
+		this.empcode = empcode;
+	}
+
+}
+// this is POJO class
